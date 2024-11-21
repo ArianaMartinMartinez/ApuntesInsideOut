831595866f3ab155773664b0224bb9cba26e2405
ArianaMartinMartinez <arianamartinmartinez@gmail.com>
2024-11-21 22:40:12 +0100

    build: routes and controllers

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\JournalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/journals', [JournalController::class, 'index'])->name('apiHome');
+Route::delete('/journals/{id}', [JournalController::class, 'destroy'])->name('apiDestroy');
+Route::post('/journals', [JournalController::class, 'store'])->name('apiStore');
+Route::put('/journals/{id}', [JournalController::class, 'update'])->name('apiUpdate');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\JournalController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [JournalController::class, 'index'])->name('home');
